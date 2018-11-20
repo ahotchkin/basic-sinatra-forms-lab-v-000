7c9f8e11388447828ef5af683fa4a3b48287d1df
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-11-20 17:46:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -11,8 +11,9 @@ class App < Sinatra::Base
     @coach = params[:coach]
     @point_guard = params[:point_guard]
     @shooting_guard = params[:shooting_guard]
-    @small_forward = params[:small_forward]
     @power_forward = params[:power_forward]
+    @small_forward = params[:small_forward]
+
     @center = params[:center]
     erb :team
   end
